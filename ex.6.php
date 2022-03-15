0f4af483b67ac332a03e778fb46c06e088eb4d4c
KateKatze <dr.katekogler@gmail.com>
2022-03-15 14:19:27 +0100

    ex. 6 done

---
+++ ex.6.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>PHP Day 2</title>
+</head>
+<body>
+    <!-- PHP -->
+    
+    <!-- /*  Exercise 6: Create a form with input fields for your firstname, lastname and age. Extract the values from each input field and display them all in a separate div. Use the POST method. If the length of your name is larger than 5 characters, change the text colour to green. Otherwise, change the text-colour to red.Now extend with adding a new input field "hobbies". Use the GET method to extract and display the hobbies */ -->
+    
+    <!-- Container -->
+    <div class="container mt-5 mb-5 p-5 border shadow text-center">
+
+        <!-- Reg. form heading -->
+        <h3 class="mb-4">Login Form</h3>
+
+        <!-- Row -->
+        <div class="row row-cols-4 justify-content-center">
+
+            <!-- Form -->
+            <form action="ex.6.php" method ="POST">
+                        <div class="mb-3">
+                                    <input type="fname" name="fname" class="form-control" id="exampleInputFname" aria-describedby="" placeholder="First Name">
+                        </div>
+                        <div class="mb-3">
+                                    <input type="lname" name="lname" class="form-control" id="exampleInputLname" aria-describedby="" placeholder="Last Name">
+                        </div>
+                        <div class="mb-3">
+                                    <input type="age" name="age" class="form-control" id="exampleInputAge" aria-describedby="" placeholder="Age">
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Log in</button>
+            </form>
+
+            <form action="ex.6.php" method ="GET">
+            <div class="mb-3">
+                        <input type="hobby" name="hobby" class="form-control" id="exampleInputHobby" aria-describedby="" placeholder="Hobby">
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Log in</button>
+            </form>
+            <!-- End of form -->
+            </div>
+            <!-- End of row -->
+        </div>
+        <!-- End of container -->
+            
+        <!-- Info -->
+        <div class="container mt-5 mb-5 p-5 border shadow text-center">
+            <h3 class="mb-4">My info</h3>
+            <div class="row row-cols-4 justify-content-center">
+               <div class="text-<?php echo $tcol ?>">
+                <?php
+                $tcol = "dark";
+
+                if( isset($_POST['submit']))
+                {
+                    $tcol = strlen($_POST["fname"]) > 5 ? "success":"danger";
+                    if( $_POST["fname"] || $_POST["lname"])
+                    {
+                        echo "<div class='text-".$tcol."'>Welcome, ". $_POST['fname']. " ". $_POST['lname']. "!". "</div> <br />";
+                    } else {
+                            echo "<div> Please insert your name and age! </div>";
+                    }
+                }
+                ?> 
+                </div>
+            
+                    <div>
+                        <?php
+                                if (isset ($_GET["submit"]))
+                                    {
+                                if ($_GET["hobby"]){
+                                echo "Your hobby is:  ". $_GET['hobby']. " ";
+                                } else {
+                                echo "Please type your hobby!";
+                                }
+                                }        
+                        ?>
+                    </div>
+            </div> 
+            </div>
+            </div>  
+
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
